ddd8400808beb076d4e17b8767d3e41849f25f95
Yousef Meska <mrbomberboy123@gmail.com>
2023-01-03 06:54:15 +0200

    :hammer: added README.md

--- go-native-docker/cmd/main.go
+++ go-native-docker/cmd/main.go
@@ -1,7 +1,14 @@
-package main 
+/**
+* Author: Yousef Meska
+* Date:
+* Summary: this project was created mainly for learning purposes, to demonstrate practically
+*		   how to create container using linux kernel features by combining them together to
+		   create a fully-isolated containers *without* Docker
+**/
+package main
 
+import "fmt"
 
-
-func main(){
-	
+func main() {
+	fmt.Println("welcome")
 }
